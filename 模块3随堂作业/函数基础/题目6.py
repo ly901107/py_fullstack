fd824f218f4c7be6daeea30b9363b118a790b3df
ly901107 <252022069@qq.com>
2017-10-30 19:29:50 +0800

    Initial commit

--- 模块3随堂作业/函数基础/题目6.py
+++ 模块3随堂作业/函数基础/题目6.py
@@ -5,5 +5,13 @@
 
 #写函数，检查字典的每一个value的长度,如果大于2，那么仅保留前两个长度的内容，并将新内容返回给调用者。
 
-def func():
-	pass
+dic = {"k1": "v1v1", "k2": [11,22,33,44]}
+
+def func(msg):
+	for k,v in msg.items():
+		if len(v) >2:
+			msg[k]=v[:2]
+	return msg
+
+res=func(dic)
+print(res)
